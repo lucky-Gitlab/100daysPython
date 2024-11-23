dc031a6c1a0861f1773fde5a8ae1559705a18cc0
lucky-Gitlab <2022uce0016@iitjammu.ac.in>
2024-11-23 20:19:49 +0530

    multivariable assign

---
+++ day19.py
@@ -0,0 +1,12 @@
+#camel casing
+luckyIsBeautiful = "iitian "
+CivilEngineering = '9/10'
+iit_jammu = "best iit "
+print(luckyIsBeautiful)
+print(CivilEngineering)
+print(iit_jammu)
+
+
+# multi variable assign 
+a , b, c = "19", 20, True
+print ("type of a ", type(a), "type of b ", type(b), "type of c ", type(c))
