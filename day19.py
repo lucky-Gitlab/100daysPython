0eca766c903cd7f9c40da12e1e3cfb1fe4e47c5f
lucky-Gitlab <2022uce0016@iitjammu.ac.in>
2024-11-23 20:30:41 +0530

    unpacking

--- day19.py
+++ day19.py
@@ -10,3 +10,19 @@ print(iit_jammu)
 # multi variable assign 
 a , b, c = "19", 20, True
 print ("type of a ", type(a), "type of b ", type(b), "type of c ", type(c))
+
+
+#assign single value to multiple variables
+x = y = z = 19
+print(x)
+print(y)
+print(z)
+
+
+
+#unpacking
+courses = ["structure ", "hydrology", "rcc"]
+x , y , z = courses
+print(x)
+print(y)
+print(z)
