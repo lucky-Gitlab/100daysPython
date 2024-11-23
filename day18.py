6ff85c3f91733ba031177d77eac4c5c1edcb88b5
lucky-Gitlab <2022uce0016@iitjammu.ac.in>
2024-11-23 19:58:46 +0530

    type of variable utilisation

---
+++ day18.py
@@ -0,0 +1,10 @@
+a = 10
+b = "case"
+c = 3.0
+d = 4.99
+e = '10'
+print("type of a ", type(a))
+print("type of b ", type(b))
+print("type of c", type(c))
+print("type of d ", type(d))
+print("type of e ", type(e))
