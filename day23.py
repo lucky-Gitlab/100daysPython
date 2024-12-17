7d30829e308472eb55cd774efcaf0787583efac1
lucky-Gitlab <2022uce0016@iitjammu.ac.in>
2024-12-17 22:35:47 +0530

    day23.py

---
+++ day23.py
@@ -0,0 +1,16 @@
+"string in python"
+a = "lucky"
+print(a)
+
+
+"multine quotes as a string"
+
+b = '''lucky is an iitian girl,
+she get 1242 rank in JEE Advanced exam '''
+
+print(b)
+
+'quotes in quotes'
+d = "lucky is an 'iitian girl' "
+e = 'lucky is an "iitian girl"'
+print(d,e)
