e8dbeb5f0bbc2fa3645c2d60f913eb4a3901cf13
lucky-Gitlab <2022uce0016@iitjammu.ac.in>
2024-12-17 23:22:11 +0530

    day23.py

--- day26.py
+++ day26.py
@@ -1,3 +1,9 @@
 "for loop in string"
 for x in "banana":
     print(x)
+
+#to get the length of a string use len()
+a = "hello  world"
+b = "lucky bhardwaj"
+print(len(a))
+print(len(b))
---
+++ day27.py
@@ -0,0 +1,7 @@
+#to check string
+#to check certain character or phrase is present in the string or not we can use keyword"in"
+sentence = "life is so beautiful after fulfulling our dreams with own money "
+print("dreams" in sentence )
+
+text = "i have started my new journey with python language to strong my resume "
+print("lucky" in text)
