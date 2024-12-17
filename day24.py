ce328c7743d24aa53a482f304427cc3f27986193
lucky-Gitlab <2022uce0016@iitjammu.ac.in>
2024-12-17 22:16:29 +0530

    day24.py

---
+++ day24.py
@@ -0,0 +1,20 @@
+"variable casting"
+
+"for integer"
+x = int(4)
+y = int(2.3)
+z = int('5')
+print(x,y,z)
+
+"for float"
+a = float(2)
+b = float(2.35)
+c = float("5.62")
+d = float("6")
+print(a,b,c,d)
+
+"for string"
+lucky = str("iitian")
+sonal =str(2)
+chirag = str(3.2)
+print(lucky, sonal, chirag)
