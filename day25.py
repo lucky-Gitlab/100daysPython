2895dbea8ceb8a56a88e03fb266cbd4ad48eaec0
lucky-Gitlab <2022uce0016@iitjammu.ac.in>
2024-12-17 22:49:10 +0530

    day25.py

---
+++ day25.py
@@ -0,0 +1,6 @@
+"sting as an array in python"
+'first letter on a positon 0 in string and each character is treated as a single character in string'
+a = "helloworld"
+print(a[1])
+print(a[2])
+print(a[9])
