e5e035ac9963eac987f8943adc93c224b8b1d64f
lucky-Gitlab <2022uce0016@iitjammu.ac.in>
2024-12-18 18:24:17 +0530

    day28.py

---
+++ day28.py
@@ -0,0 +1,10 @@
+#slicing of stringss in python
+b = "hello, worlds"
+print(b[2:5])
+
+A = "LUCKYBHARDWAJ IS AN IITIAN OF IIT JAMMU "
+print(A[3:6])
+
+
+f = "mymomismy life , everything"
+print(f[4:9])
