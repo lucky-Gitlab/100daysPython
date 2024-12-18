8d79283a96bcd1627fae2ed737729dbd99021cbf
lucky-Gitlab <2022uce0016@iitjammu.ac.in>
2024-12-18 18:54:00 +0530

    day30.py

---
+++ day30.py
@@ -0,0 +1,15 @@
+#upper case in python
+a = "hello, world"
+print(a.upper())
+
+#lower case in python
+b = "LUCKY BHARDWAJ"
+print(b.lower())
+
+#to remove whitespace
+c = "  hello world  "
+print(a.strip())
+
+
+d = "lucky is a good girl"
+print(d.strip())
