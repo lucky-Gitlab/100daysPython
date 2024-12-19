462dd0d94e75873f374e4c0c775579416874e3c4
lucky-Gitlab <2022uce0016@iitjammu.ac.in>
2024-12-19 22:42:06 +0530

    day36.py

---
+++ day36.py
@@ -0,0 +1,6 @@
+# a placeholder can contain python code, like math operations
+text = f"a heater cost is increased by {20*5}"
+print(text)
+
+txt = f"my institute fee in 6th semester is {50000+3000}"
+print(txt)
