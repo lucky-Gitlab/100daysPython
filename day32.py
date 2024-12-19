3566db4d1a130929b7b08b859e0cf361d80e480f
lucky-Gitlab <2022uce0016@iitjammu.ac.in>
2024-12-19 21:54:13 +0530

    day32.py

---
+++ day32.py
@@ -0,0 +1,16 @@
+#string concentenation
+a = "lucky"
+b = "bhardwaj"
+print(a+b)
+
+
+c = "lucky"
+d = "bhardwaj"
+e = a+b
+print(e)
+
+# to add a space between two strings 
+A = "lucky"
+B = "bhardwaj"
+C = A+ " " +B
+print(C)
