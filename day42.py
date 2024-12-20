47a9c13677aaca53dbc849fccaad4a2d199b32f5
lucky-Gitlab <2022uce0016@iitjammu.ac.in>
2024-12-20 23:25:49 +0530

    day42.py

---
+++ day42.py
@@ -0,0 +1,13 @@
+#some built in function like isinstance
+a = 19
+print(isinstance(a,int))
+
+b = 4.00
+print(isinstance(b,float))
+      
+      
+c = True
+print(isinstance(c,bool))
+
+d = 0
+print(isinstance(d, bool))
